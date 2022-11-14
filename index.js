fee6af9db65daecc39020a9fce338cb47de48b64
Janvier Barreto Acevedo <janvieracevedo912gmail.com>
2022-11-14 15:49:28 -0600

    Wrote logic to make the program run

---
+++ index.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+const displayMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+const licenseChoices = ['MIT', 'Apache 2.0', 'GPLv3', 'ISC', 'none']
+
+const writeToFile = (data) => 
+displayMarkdown.generateMarkdown(data);
+
+// INIT FUNCTION
+function init() {
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license are you using for your project?',
+      choices: licenseChoices,
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Write a short description of the project: ',
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'What commands should be run to install the dependencies for this project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Give a brief description of how your project can be used: ',
+    },
+    {
+      type: 'input',
+      name: 'contributions',
+      message: 'How can people contribute to the project: ',
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'What commands should be use to run tests on the project?',
+    },
+  ])
+  .then((data) => {
+    const readmeContent = writeToFile(data);
+    fs.writeFile('./generatedReadMe/README.md', readmeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created the README.md')
+    );
+
+  });
+}
+
+// RUNS PROGRAM
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,81 @@
+
+const backticks = "```"
+var licenseStatus = false
+var githubLicense = ``
+
+//MAKING BADGE ON TOP OR LEAVING EMPTY IF CHOOSE NONE
+function displayLicensebadge(license) {
+  if (license == "none") {
+    return githubLicense = ""
+  } else {
+    licenseStatus = true
+    displayLicenseLink(license)
+  }
+}
+
+//LINK FOR THE BADGE
+function displayLicenseLink(license) {
+  let licenseLink = `https://img.shields.io/badge/license-${license}-green`
+  githubLicense = `![Github License](${licenseLink})`
+  displayLicenseSection(license);
+  return githubLicense
+}
+
+//LICENSE SECTION
+function displayLicenseSection(license) {
+  if (licenseStatus) {
+    return licenseSection = `This project is licensed under the ${license} license.`
+  } else {
+    return licenseSection = `This project is using no license.`
+  }
+}
+
+//GENERATES THE README CONTENTS
+function generateMarkdown({ github, email, license, title, description, install, usage, contributions, testing }) {
+  displayLicensebadge(license);
+  displayLicenseSection(license);
+return `# ${title}
+${githubLicense}
+## Description
+${description}
+___
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+___
+## Installation
+To install the needed dependencies, run the following commands:
+${backticks}
+${install} 
+${backticks}
+___
+## Usage
+${usage}
+___
+## License
+${licenseSection}
+___
+## Contributing
+${contributions}
+___
+## Tests
+To test the project, run the following commands:
+${backticks}
+${testing}
+${backticks}
+___
+## Questions
+If you have any questions feel free tp contact me directly at ${email}. You can find my github profile at [github.com/${github}](https://github.com/${github}/)
+`;
+}
+
+module.exports = {
+    generateMarkdown,
+    displayLicensebadge,
+    displayLicenseLink,
+    displayLicenseSection,
+};
